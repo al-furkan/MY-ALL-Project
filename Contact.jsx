3aba2396f74defbf2a1b21f83af187ae70493e65
MD AL FURKAN <103766452+al-furkan@users.noreply.github.com>
2023-03-10 23:33:23 +0600

    cv file
    cv file

---
+++ Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Contact.css'
+const Contact=() =>{
+  return (
+    <section id='contact'>
+    contact
+    </section>
+  )
+}
+
+export default Contact
---
+++ Test.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './test.css'
+const test = () => {
+  return (
+    <section id='test'>test</section>
+  )
+}
+
+export default test;
